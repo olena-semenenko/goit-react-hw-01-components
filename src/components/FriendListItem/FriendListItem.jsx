1ec4db768e37d42c9abcb6137f7379a6876b3878
olena-semenenko <elen.kinakh@gmail.com>
2023-02-22 20:05:35 +0100

    Update FriendListItem.jsx

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,11 +1,11 @@
-import PropTypes from 'prop-types'
-// import friends from 'friends.js'
-export const FriendListItem = ({ avatar, name, isOnline }) => {
-    return ()
-}
+// import PropTypes from 'prop-types'
+// // import friends from 'friends.js'
+// export const FriendListItem = ({ avatar, name, isOnline }) => {
+//     return ()
+// }
 
-FriendListItem.propTypes = {
-    avatar: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    isOnline: PropTypes.bool.isRequired,
-}
+// FriendListItem.propTypes = {
+//     avatar: PropTypes.string.isRequired,
+//     name: PropTypes.string.isRequired,
+//     isOnline: PropTypes.bool.isRequired,
+// }
